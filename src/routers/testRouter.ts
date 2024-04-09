9827ea45456d28b7db7463f94edeb0ba51e2b27a
keylime7 <keylime7@cau.ac.kr>
2024-04-09 14:13:57 +0900

    feat: git clone 기능

--- src/routers/testRouter.ts
+++ src/routers/testRouter.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import wrapAsync from '../helpers/wrapFunction';
 import {BaseResponseStatus} from '../helpers/baseResponseStatus';
 import response from '../helpers/response';
+import TestService from '../services/testService';
 
 const router = Router();
 
@@ -20,7 +21,21 @@ router.get('/', wrapAsync(async (req: Request, res: Response) => {
  *  post: /test/zipUpload
  */
 router.post('/zipUpload', wrapAsync(async (req: Request, res: Response) => {
-  
+
+}));
+
+/**
+ *  git clone test api
+ *  post: /test/clone
+ *  body: repositoryURL
+ */
+router.post('/clone', wrapAsync(async (req: Request, res: Response) => {
+  const { repositoryURL } = req.body;
+
+  const gitCloneResponse = await TestService.gitClone(repositoryURL);
+  const responseStatus = BaseResponseStatus.SUCCESS;
+
+  return res.status(responseStatus.status).json(response(responseStatus, gitCloneResponse));
 }));
 
 export default router;
---
+++ src/services/testService.ts
@@ -0,0 +1,21 @@
+import simpleGit from 'simple-git';
+
+class TestService {
+  async gitClone(repositoryURL: string) {
+    const parsedURL = new URL(repositoryURL);
+    const pathParts = parsedURL.pathname.split('/').filter((part) => part !== '');
+    const username = pathParts[0];
+    const repositoryName = pathParts[1].replace('.git', '');
+
+    const git = simpleGit();
+    try {
+      git.clone(repositoryURL, './copySourceCode/' + username + '/' + repositoryName);
+    } catch(error) {
+      console.error(error);
+    }
+
+    return;
+  }
+}
+
+export default new TestService();
