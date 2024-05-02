76d36ac4cb997eb593ce9d4736f5ebb9d70ad26f
keylime7 <keylime7@cau.ac.kr>
2024-05-02 13:45:27 +0900

    feat: zip 업로드 api

--- src/routers/testRouter.ts
+++ src/routers/testRouter.ts
@@ -3,6 +3,7 @@ import wrapAsync from '../helpers/wrapFunction';
 import {BaseResponseStatus} from '../helpers/baseResponseStatus';
 import response from '../helpers/response';
 import TestService from '../services/testService';
+import upload from '../middlewares/multerMiddleware';
 
 const router = Router();
 
@@ -20,8 +21,14 @@ router.get('/', wrapAsync(async (req: Request, res: Response) => {
  *  zip 압축 소스코드 업로드 test api
  *  post: /test/zipUpload
  */
-router.post('/zipUpload', wrapAsync(async (req: Request, res: Response) => {
+router.post('/zipUpload', upload.single('zipFile'), wrapAsync(async (req: Request, res: Response) => {
+  if(!req.file) {
+    const responseStatus = BaseResponseStatus.ERROR;
+    return res.status(responseStatus.status).json(response(responseStatus));
+  }
 
+  const responseStatus = BaseResponseStatus.SUCCESS;
+  return res.status(responseStatus.status).json(response(responseStatus));
 }));
 
 /**
