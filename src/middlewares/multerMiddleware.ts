974c3fc7a0f5aaae98eb97387058652ef2403eb8
keylime7 <keylime7@cau.ac.kr>
2024-05-02 13:40:48 +0900

    feat: multer 미들웨어

---
+++ src/middlewares/multerMiddleware.ts
@@ -0,0 +1,17 @@
+import multer from 'multer';
+import path from 'path';
+
+const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadFolder);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+export default upload;
