1f90ceb417612aef85e076a8440cbe103b453c5d
keylime7 <keylime7@cau.ac.kr>
2024-05-02 14:27:00 +0900

    feat: git clone 후 zip 압축

--- src/services/testService.ts
+++ src/services/testService.ts
@@ -1,6 +1,34 @@
 import simpleGit from 'simple-git';
+import JSZip from 'jszip';
+import { promises as fs } from 'fs';
+import path from 'path';
 
 class TestService {
+  async addDirectoryToZip(zip: JSZip, dirPath: string, basePath: string = '') {
+    const files = await fs.readdir(dirPath);
+
+    for(const file of files) {
+      const filePath = path.join(dirPath, file);
+      const stat = await fs.stat(filePath);
+
+      if(stat.isDirectory()) {
+        const subDir = zip.folder(path.join(basePath, file))!;
+        await this.addDirectoryToZip(subDir, filePath, path.join(basePath, file));
+      } else {
+        const content = await fs.readFile(filePath);
+        zip.file(path.join(basePath, file), content);
+      }
+    }
+  }
+
+  async compressFolder(dirPath: string) {
+    const zip = new JSZip();
+    await this.addDirectoryToZip(zip, dirPath);
+
+    const zipContent = await zip.generateAsync({ type: 'nodebuffer' });
+    await fs.writeFile(dirPath + '.zip', zipContent);
+  }
+
   async gitClone(repositoryURL: string) {
     const parsedURL = new URL(repositoryURL);
     const pathParts = parsedURL.pathname.split('/').filter((part) => part !== '');
@@ -9,6 +37,7 @@ class TestService {
 
     const git = simpleGit();
     await git.clone(repositoryURL, 'copySourceCode/' + username + '/' + repositoryName);
+    await this.compressFolder('copySourceCode/' + username + '/' + repositoryName);
 
     return;
   }
