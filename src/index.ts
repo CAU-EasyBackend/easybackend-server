c5a362a6bee48ab3164249a97ae0242d1f837fc6
keylime7 <keylime7@cau.ac.kr>
2024-05-05 16:08:19 +0900

    feat: db 작업

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import configureEnvironment from './config/environment';
 import configureExpressApp from './config/expressServer';
+import connectDatabase from './config/database';
 
 configureEnvironment();
+connectDatabase();
 
 const PORT = process.env.PORT || 80;
 const server = configureExpressApp();
