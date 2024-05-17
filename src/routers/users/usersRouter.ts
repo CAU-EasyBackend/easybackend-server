29e6a8b436dedaaee60b0c4cb0fd9e4c588521a1
keylime7 <keylime7@cau.ac.kr>
2024-05-17 19:49:14 +0900

    feat: /users/:userId/deployments api 라우팅을 /deployments로 간소화함

--- src/routers/users/usersRouter.ts
+++
@@ -1,8 +0,0 @@
-import {Request, Response, Router} from 'express';
-import deployInfosRouter from './deployInfosRouter';
-
-const router = Router();
-
-router.use('/:userId/deployInfos', deployInfosRouter);
-
-export default router;
