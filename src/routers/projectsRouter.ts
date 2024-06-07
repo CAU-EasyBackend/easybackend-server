b345911439045d91e4e98f742a52e2678eb82d80
keylime7 <keylime7@cau.ac.kr>
2024-06-07 16:29:09 +0900

    feat: 기존 yaml 파일 형식으로 api명세 저장, 불러오기하던거 엔드포인트 수정했고, 기존 엔드포인트에는 yaml 파일이 아닌 api 객체 주고 받는 것으로 수정

--- src/routers/projectsRouter.ts
+++ src/routers/projectsRouter.ts
@@ -6,6 +6,7 @@ import {BaseResponseStatus} from '../helpers/baseResponseStatus';
 import response from '../helpers/response';
 import upload from '../middlewares/multerMiddleware';
 import fs from 'fs';
+import { OpenAPI } from "openapi-types";
 
 const router = Router();
 
@@ -23,10 +24,24 @@ router.get('/list', isAuthenticated, wrapAsync(async (req: Request, res: Respons
 }));
 
 /**
- * api 명세 yaml 조회 api
+ * api 명세 객체 조회 api
  * get: /api/projects/:projectId/apiSpec
  */
 router.get('/:projectId/apiSpec', isAuthenticated, wrapAsync(async (req: Request, res: Response) => {
+  const userId = req.user!.userId;
+  const projectId = req.params.projectId;
+
+  const responseStatus = BaseResponseStatus.SUCCESS;
+  const apiSpec = await ProjectsService.getApiSpec(userId, projectId);
+
+  return res.status(responseStatus.status).json(response(responseStatus, {api: apiSpec.apiObject}));
+}));
+
+/**
+ * api 명세 yaml 파일 조회 api
+ * get: /api/projects/:projectId/apiSpec/yaml
+ */
+router.get('/:projectId/apiSpec/yaml', isAuthenticated, wrapAsync(async (req: Request, res: Response) => {
   const userId = req.user!.userId;
   const projectId = req.params.projectId;
 
@@ -42,7 +57,23 @@ router.get('/:projectId/apiSpec', isAuthenticated, wrapAsync(async (req: Request
  * put: /api/projects/apiSpec/save
  * body: projectName
  */
-router.put('/apiSpec/save', isAuthenticated, upload.single('yamlFile'), wrapAsync(async (req: Request, res: Response) => {
+router.put('/apiSpec/save', isAuthenticated, wrapAsync(async (req: Request, res: Response) => {
+  const userId = req.user!.userId;
+  const projectName = req.body.projectName;
+  const apiObject: OpenAPI.Document = req.body.api;
+
+  const responseStatus = BaseResponseStatus.SUCCESS;
+  await ProjectsService.saveApiSpec(userId, projectName, apiObject);
+
+  return res.status(responseStatus.status).json(response(responseStatus));
+}));
+
+/**
+ * api 명세서 yaml 파일로 저장 api
+ * put: /api/projects/apiSpec/save/yaml
+ * body: projectName
+ */
+router.put('/apiSpec/save/yaml', isAuthenticated, upload.single('yamlFile'), wrapAsync(async (req: Request, res: Response) => {
   if(!req.file) {
     const responseStatus = BaseResponseStatus.YAML_UPLOAD_ERROR;
     return res.status(responseStatus.status).json(response(responseStatus));
@@ -55,7 +86,7 @@ router.put('/apiSpec/save', isAuthenticated, upload.single('yamlFile'), wrapAsyn
   const yamlContent = fs.readFileSync(yamlFilePath, 'utf-8');
 
   const responseStatus = BaseResponseStatus.SUCCESS;
-  await ProjectsService.saveApiSpec(userId, projectName, yamlContent);
+  await ProjectsService.saveApiSpecYaml(userId, projectName, yamlContent);
 
   return res.status(responseStatus.status).json(response(responseStatus));
 }));
