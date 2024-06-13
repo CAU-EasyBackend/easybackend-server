936cc98ba2cb54cb1da63c08cfe25af6a3022dff
keylime7 <keylime7@cau.ac.kr>
2024-06-13 16:22:32 +0900

    feat: js 컴파일 산출물 삭제

--- src/routers/users/usersRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var deployInfosRouter_1 = require("./deployInfosRouter");
-var router = (0, express_1.Router)();
-router.use('/:userId/deployInfos', deployInfosRouter_1.default);
-exports.default = router;
